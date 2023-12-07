85e7a23af1eac99dd5c43d7a310112c07409ac5f
Arnaldo Junior <abs.junnior@hotmail.com>
2023-12-06 21:39:53 -0300

    Definição da Model p/ consumo da API

---
+++ lib/src/model/books_model.dart
@@ -0,0 +1,28 @@
+class BooksModel {
+  int? id;
+  String? title;
+  String? author;
+  String? coverUrl;
+  String? downloadUrl;
+
+  BooksModel(
+      {this.id, this.title, this.author, this.coverUrl, this.downloadUrl});
+
+  BooksModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    title = json['title'];
+    author = json['author'];
+    coverUrl = json['cover_url'];
+    downloadUrl = json['download_url'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['title'] = title;
+    data['author'] = author;
+    data['cover_url'] = coverUrl;
+    data['download_url'] = downloadUrl;
+    return data;
+  }
+}
