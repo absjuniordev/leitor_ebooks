4e9198b9a9ef77516839822c3e9cce07d8575afc
Arnaldo Junior <abs.junnior@hotmail.com>
2023-12-12 22:48:28 -0300

    Ajuste da forma de definir os favoritos

--- lib/src/page/home_page.dart
+++ lib/src/page/home_page.dart
@@ -22,6 +22,7 @@ class _HomePageState extends State<HomePage> {
   );
   var booksModel = <BooksModel>[];
   List<BooksModel> favoriteBooks = [];
+  bool isFavorite = false;
 
   @override
   void initState() {
@@ -56,72 +57,166 @@ class _HomePageState extends State<HomePage> {
             style: TextStyle(color: Colors.white),
           ),
           backgroundColor: const Color.fromARGB(255, 7, 85, 255),
-        ),
-        body: GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              crossAxisSpacing: 2,
-              mainAxisSpacing: 10,
-              childAspectRatio: 0.8),
-          itemCount: booksModel.length,
-          itemBuilder: (_, index) {
-            var book = booksModel[index];
-            return Card(
-              elevation: 3,
-              child: InkWell(
-                onTap: () async {
-                  String epubFilePath = await dioServiceImp.downloadDio(book);
-                  if (epubFilePath.isNotEmpty) {
-                    await vocsyEpubOpen.openDownloadedEpub(epubFilePath);
-                  }
-                },
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        IconButton(
-                          icon: Icon(
-                            favoriteBooks.contains(book)
-                                ? Icons.bookmark
-                                : Icons.bookmark_border,
-                            color: favoriteBooks.contains(book)
-                                ? const Color.fromARGB(255, 158, 12, 1)
-                                : null,
-                          ),
-                          onPressed: () {
-                            toggleFavorite(book);
-                          },
-                        ),
-                      ],
+          actions: [
+            InkWell(
+              onTap: () {
+                setState(() {
+                  isFavorite = !isFavorite;
+                });
+              },
+              child: Container(
+                color: isFavorite ? Colors.green : Colors.white,
+                width: 80,
+                height: 35,
+                child: const Center(
+                  child: Text(
+                    "Favoritos",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
-                    Expanded(
-                      child: Image.network(
-                        book.coverUrl.toString(),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      book.title.toString(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      book.author.toString(),
-                      style: const TextStyle(
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
-            );
-          },
+            ),
+          ],
         ),
+        body: isFavorite == false
+            ? GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 2,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 0.8,
+                ),
+                itemCount: booksModel.length,
+                itemBuilder: (_, index) {
+                  var book = booksModel[index];
+                  return Card(
+                    elevation: 3,
+                    child: InkWell(
+                      onTap: () async {
+                        String epubFilePath =
+                            await dioServiceImp.downloadDio(book);
+                        if (epubFilePath.isNotEmpty) {
+                          await vocsyEpubOpen.openDownloadedEpub(epubFilePath);
+                        }
+                      },
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Spacer(),
+                              IconButton(
+                                iconSize: 40,
+                                icon: Icon(
+                                  favoriteBooks.contains(book)
+                                      ? Icons.star
+                                      : Icons.star_border,
+                                  color: favoriteBooks.contains(book)
+                                      ? const Color.fromARGB(255, 158, 12, 1)
+                                      : null,
+                                ),
+                                onPressed: () {
+                                  toggleFavorite(book);
+                                },
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: Image.network(
+                              book.coverUrl.toString(),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            book.title.toString(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            book.author.toString(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              )
+            : GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 2,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 0.8,
+                ),
+                itemCount: favoriteBooks.length,
+                itemBuilder: (_, index) {
+                  var book = favoriteBooks[index];
+                  return Card(
+                    elevation: 3,
+                    child: InkWell(
+                      onTap: () async {
+                        String epubFilePath =
+                            await dioServiceImp.downloadDio(book);
+                        if (epubFilePath.isNotEmpty) {
+                          await vocsyEpubOpen.openDownloadedEpub(epubFilePath);
+                        }
+                      },
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              const Spacer(),
+                              IconButton(
+                                iconSize: 40,
+                                icon: Icon(
+                                  favoriteBooks.contains(book)
+                                      ? Icons.star
+                                      : Icons.star_border,
+                                  color: favoriteBooks.contains(book)
+                                      ? const Color.fromARGB(255, 158, 12, 1)
+                                      : null,
+                                ),
+                                onPressed: () {
+                                  toggleFavorite(book);
+                                },
+                              ),
+                            ],
+                          ),
+                          Expanded(
+                            child: Image.network(
+                              book.coverUrl.toString(),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            book.title.toString(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            book.author.toString(),
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
       ),
     );
   }
